a140ad162d24ca025e2f1a8ca036b33985c0e73a
faizansaghir <dbz.rashidsaghir@gmail.com>
2025-01-26 21:50:37 +0530

    Added logic to check for winner and lifted necessary derived state

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import GameBoard from './components/GameBoard'
 import { useState } from 'react'
 import Log from './components/Log'
 
+import {WINNING_COMBINATIONS} from './winning-combinations'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
   if(gameTurns.length>0 && gameTurns[0].player === 'X')
@@ -14,6 +22,25 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns)
+
+  let gameBoard = initialGameBoard;
+
+  for(const turn of gameTurns){
+      const {square, player} = {...turn}
+      const {row, col} = {...square}
+      gameBoard[row][col] = player
+  }
+
+  let winner = null
+
+  for(const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+    const SecondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+    if(firstSquareSymbol && firstSquareSymbol==SecondSquareSymbol && firstSquareSymbol==thirdSquareSymbol)
+      winner = firstSquareSymbol
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
@@ -30,7 +57,8 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={activePlayer==='X'}/>
           <Player name="Player 2" symbol="O" isActive={activePlayer==='O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} turns={gameTurns}/>
+        {winner && <p>You Won, {winner}</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} board={gameBoard}/>
       </div>
       <Log turns={gameTurns}/>
     </main>
